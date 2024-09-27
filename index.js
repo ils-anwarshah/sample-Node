ec22b9f230eee54827be6a1733ceb2ebefe56ebd
ils-Anwarshah <a.shah@thesynapses.com>
2024-09-27 13:25:14 +0530

    changaed

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { CONFIG } = require("./getSecrets");
 const app = express();
 
 console.log("CHECK CONFIG", CONFIG);
+console.log("CHECK CONFIG>>>>", CONFIG.SECRETS);
 
 app.get("/status", (req, res) => {
   res.json({ status: "Working", NAME: CONFIG?.APP_NAME });
