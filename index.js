0bdfa34730a2fb655bf97c033b0d78787ff7428f
ils-Anwarshah <a.shah@thesynapses.com>
2024-09-27 13:12:30 +0530

    changhed

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const http = require("http");
 const { CONFIG } = require("./getSecrets");
 const app = express();
 
+console.log("CHECK CONFIG", CONFIG);
+
 app.get("/status", (req, res) => {
   res.json({ status: "Working", NAME: CONFIG?.APP_NAME });
 });
